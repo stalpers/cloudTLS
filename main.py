ee5f132fe5cc2b1a18f277743fdb35785c644f2d
Stefan <stefan@MacBook-Air-von-Tanja.local>
2023-04-08 16:13:42 +0200

    ...

---
+++ main.py
@@ -0,0 +1,46 @@
+import sys
+import json
+import argparse
+
+
+SAN = [
+    'dns_names',
+    'ip_addresses'
+]
+
+
+def parse_sslyze(j):
+    print("Start parsing...")
+    for i in j['server_scan_results']:
+        # original Hostname / IP
+        print(i['server_location']['hostname'])
+        print(i['server_location']['ip_address'])
+        print(i['server_location']['port'])
+
+        # Subject Alternative names
+        j_SAN = i['scan_result']['certificate_info']['result']['certificate_deployments'][0]['path_validation_results'][0]['verified_certificate_chain'][0]['subject_alternative_name']
+
+        print(j_SAN['dns_names'])
+        print(j_SAN['ip_addresses'])
+
+
+if __name__ == "__main__":
+    p_args = argparse.ArgumentParser(description='Cloud Security Scanner')
+    p_args.add_argument(metavar='filename.json', dest='in_file')
+    args = p_args.parse_args()
+
+    try:
+        d = json.loads(open(args.in_file, 'r').read())
+    except:
+        print("%s: error opening file %s" % (sys.argv[0], args.inf))
+        sys.exit()
+
+    parse_sslyze(d)
+
+    # subject_alternative_name
+    ### dns_names
+    ### ip_addresses
+
+    # verified_certificate_chain => subject => Attributes => value
+
+    # hostname_used_for_server_name_indication
--- scan.sh
+++ scan.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 export TESTING=1
@@ -9,15 +8,16 @@ export TEST_DATA=./testing
 export AWS_IP=aws_ip
 export AZURE_IP=azure_ip
 export GCP_IP=gcp_ip
-export OCI_IP=ci_ip
+export OCI_IP=oci_ip
 export JS=json
 export TXT=txt
 export IP_LIST=ips.txt
 export SCAN_OUT=scan.out
 
-export SCAN_CLEAN=scan.txt
+export SCAN_CLEAN_AWS=scan-aws.txt
+export SCAN_CLEAN_GCP=scan-gcp.txt
 export CERT_BUNDLE=/home/stefan/tools/ca-bundle.crt
-export TLS_INFO=tls.json
+
 
 export AWS_URL=https://ip-ranges.amazonaws.com/ip-ranges.json
 export AZURE_URL=https://download.microsoft.com/download/7/1/D/71D86715-5596-4529-9B13-DA13A5DE5B63/ServiceTags_Public_20230313.json
@@ -36,34 +36,48 @@ if [ $TESTING -eq 1 ]; then
 else
 	echo "[SCAN] Download data"
 	echo "[SCAN] AWS"
-	curl $AWS_URL -o $IP_DIR/$AWS_IP.$JS
+	curl $AWS_URL -s -o $IP_DIR/$AWS_IP.$JS
 	echo "[SCAN] Azure"
-	curl $AZURE_URL -o $IP_DIR/$AZURE_IP.$JS
+	curl $AZURE_URL -s -o $IP_DIR/$AZURE_IP.$JS
 	echo "[SCAN] GCP"
-	curl $GCP_URL -o $IP_DIR/$GCP_IP.$JS
+	curl $GCP_URL -s -o $IP_DIR/$GCP_IP.$JS
 	echo "[SCAN] OCI"	
-	curl $OCI_URL -o $IP_DIR/$OCI_IP.$JS
+	curl $OCI_URL -s -o $IP_DIR/$OCI_IP.$JS
 fi
 
 echo "[SCAN] Parsing"
 cat $IP_DIR/$AWS_IP.$JS | jq '.prefixes[] | .ip_prefix' | sed -e 's/"//g' > $IP_DIR/$AWS_IP.$TXT
 cat $IP_DIR/$GCP_IP.$JS |  jq '.prefixes [] | .ipv4Prefix' | sed -e 's/"//g' | grep -v null > $IP_DIR/$GCP_IP.$TXT
 
+if [ $TESTING -eq 1 ]; then
+
+  cp $IP_DIR/$AWS_IP.$TXT /tmp/$AWS_IP.$TXT
+  head -1 /tmp/$AWS_IP.$TXT > $IP_DIR/$AWS_IP.$TXT
+  cp $IP_DIR/$GCP_IP.$TXT /tmp/$GCP_IP.$TXT
+  head -1 /tmp/$GCP_IP.$TXT > $IP_DIR/$GCP_IP.$TXT
+
+  wc -l $IP_DIR/$AWS_IP.$TXT
+  wc -l $IP_DIR/$GCP_IP.$TXT
+
+fi
+
 # Azure JQ
 # cat data/azure_ip.json|jq -c '.values[]| select(.name | contains("AzureCloud")) | .properties.addressPrefixes[]'
 
-echo counting total number of IP addresses - this can take some time... 
-export ip_count=`nmap -n -sL -iL data/ips.txt | wc -l`
 
-echo Scanning $ip_count IP addressses for SSL parameters
-sudo masscan -p443 --rate 100000 -iL $IP_DIR/$IP_LIST -oL $IP_DIR/$SCAN_OUT 
-cat $IP_DIR/$SCAN_OUT | awk {'print $4'} | awk NF | sort -u > $IP_DIR/$SCAN_CLEAN
+echo Scanning $ip_count AWS IP addressses for SSL parameters
+sudo masscan -p443 --rate 100000 -iL $IP_DIR/$AWS_IP.$TXT -oL $IP_DIR/$SCAN_OUT
+cat $IP_DIR/$SCAN_OUT | awk {'print $4'} | awk NF | sort -u > $IP_DIR/$SCAN_CLEAN_AWS
+
+echo Scanning $ip_count GCP IP addressses for SSL parameters
+sudo masscan -p443 --rate 100000 -iL $IP_DIR/$GCP_IP.$TXT -oL $IP_DIR/$SCAN_OUT
+cat $IP_DIR/$SCAN_OUT | awk {'print $4'} | awk NF | sort -u > $IP_DIR/$SCAN_CLEAN_GCP
 
 echo Performing TLS scans
-cat $IP_DIR/$SCAN_CLEAN | tls-scan --port=443 --concurrency=150 --cacert=$CERT_BUNDLE 2>/dev/null -o $IP_DIR/$TLS_INFO
 
-sslyze  --certinfo --json_out=tls.json --targets_in=./testing/test_targets.txt
+echo "[TLS] AWS"
+sslyze  --certinfo --json_out=tls_aws.json --targets_in=$IP_DIR/$SCAN_CLEAN_AWS --quiet
+echo "[TLS] GCP"
+sslyze  --certinfo --json_out=tls_gcp.json --targets_in=$IP_DIR/$SCAN_CLEAN_GCP --quiet
+
 cat tls.json| jq -c '.server_scan_results[] |  select  (.network_configuration.tls_server_name_indication | contains ("193.99.144.80"))
-
-# .server_scan_results[] |  select  (.network_configuration.tls_server_name_indication | contains ("193.99.144.80")) |.scan_result.certificate_info.result.certificate_deployments[].path_validation_results[].verified_certificate_chain
-
